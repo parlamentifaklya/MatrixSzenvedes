621239333465a167ca5603b2d377ae8f22ed1cac
parlamentifaklya <20.kokenyesi.andor@mechwart.com>
2024-11-25 12:35:38 +0100

    Add project files.

---
+++ MatrixSzenvedes/Interface/IMatrixOperations.cs
@@ -0,0 +1,25 @@
+﻿using MatrixSzenvedes.AbsClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixSzenvedes.Interface
+{
+    internal interface IMatrixOperations
+    {
+        public void Fill(double value);
+        public void FillRandomFloat(double start, double end);
+        public void FillRandomInt(int start, int end);
+        public void Add(AbsMatrix addMatrix);
+        public void Sub(AbsMatrix subMatrix);
+        public bool Equals(AbsMatrix other);
+
+        public void RotateToLeft();
+        public void RotatetToRight();
+        public void RotateToTop();
+        public void RotateToDown();
+
+    }
+}
---
+++ MatrixSzenvedes/MyMatrix/LakatosMatrix.cs
@@ -0,0 +1,120 @@
+﻿using MatrixSzenvedes.AbsClass;
+using MatrixSzenvedes.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixSzenvedes.MyMatrix
+{
+    public class LakatosMatrix : AbsMatrix, IMatrixOperations
+    {
+        public void Add(AbsMatrix addMatrix)
+        {
+            if (this.ColumnCount != addMatrix.ColumnCount || this.RowCount != addMatrix.RowCount)
+            {
+                Console.WriteLine("Matrices must have the same dimensions!");
+                return;
+            }
+
+            for (int rowIndex = 0; rowIndex < RowCount; rowIndex++)
+            {
+                for (int colIndex = 0; colIndex < ColumnCount; colIndex++)
+                {
+                    this[rowIndex, colIndex] += addMatrix[rowIndex, colIndex];
+                }
+            }
+        }
+
+        public bool Equals(AbsMatrix other)
+        {
+            if (this.ColumnCount != other.ColumnCount || this.RowCount != other.RowCount)
+            {
+                return false;
+            }
+
+            for (int rowIndex = 0; rowIndex < RowCount; rowIndex++)
+            {
+                for (int colIndex = 0; colIndex < ColumnCount; colIndex++)
+                {
+                    if (this[rowIndex, colIndex] != other[rowIndex, colIndex]) 
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public void Fill(double value)
+        {
+            for (int rowIndex = 0; rowIndex < RowCount; rowIndex++)
+            {
+                for (int colIndex = 0; colIndex < ColumnCount; colIndex++)
+                {
+                    this[rowIndex, colIndex] = value;
+                }
+            }
+        }
+
+        public void FillRandomFloat(double start, double end)
+        {
+            Random rnd = new Random();
+
+            for (int rowIndex = 0; rowIndex < RowCount; rowIndex++)
+            {
+                for (int colIndex = 0; colIndex < ColumnCount; colIndex++)
+                {
+                    this[rowIndex, colIndex] = (float)(start + (rnd.NextDouble() * (end - start)));
+                }
+            }
+        }
+
+        public void FillRandomInt(int start, int end)
+        {
+            Random rnd = new Random();
+
+            for (int rowIndex = 0; rowIndex < RowCount; rowIndex++)
+            {
+                for (int colIndex = 0; colIndex < ColumnCount; colIndex++)
+                {
+                    this[rowIndex, colIndex] = rnd.NextInt64(start, end);
+                }
+            }
+        }
+
+        public void RotateToDown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RotateToLeft()
+        {
+            for (int rowIndex = 0; rowIndex < RowCount; rowIndex++)
+            {
+                double temp = this[rowIndex, 0];
+                for (int colIndex = 0; colIndex < ColumnCount; colIndex++)
+                {
+                    this[rowIndex, colIndex - 1] = this[rowIndex, colIndex];
+                }
+                this[rowIndex, RowCount - 1] = temp;
+            }
+        }
+
+        public void RotateToTop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RotatetToRight()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Sub(AbsMatrix subMatrix)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
